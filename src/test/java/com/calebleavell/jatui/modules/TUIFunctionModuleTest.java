b259a8db43905192e7c9776bf9508a34a4b93989
Caleb Leavell <calebjleavell06@gmail.com>
2025-08-28 00:55:57 -0700

    create TUIFunctionModuleTest, remove .idea from gitignore

---
+++ src/test/java/com/calebleavell/jatui/modules/TUIFunctionModuleTest.java
@@ -0,0 +1,20 @@
+package com.calebleavell.jatui.modules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TUIFunctionModuleTest {
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void getFunction() {
+    }
+
+    @Test
+    void setFunction() {
+    }
+}
