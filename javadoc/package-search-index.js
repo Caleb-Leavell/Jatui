494f09efef8974062e5f9a9b502d5c5f39e4bede
Caleb Leavell <calebjleavell06@gmail.com>
2026-01-08 22:24:58 -0700

    create JavaDoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.calebleavell.jatui.core"},{"l":"com.calebleavell.jatui.modules"},{"l":"com.calebleavell.jatui.templates"},{"l":"com.calebleavell.jatui.util"}];updateSearchResults();
